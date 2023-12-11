89c1b565c9a601cce4b8c6def5fcd069ec490b12
Daniel <danie1500956@IN-TR5LPW110471>
2023-12-11 16:39:20 +0530

    Rework on Constant

--- src/main/java/constants/Constants.java
+++ src/main/java/constants/Constants.java
@@ -5,10 +5,13 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class Constants {
 
 	
-	public static final String datafilepath = "C:\\Users\\daniel.r\\eclipse-workspace\\CucumberFramework\\src\\test\\resources\\TData.xlsx";
+	public static final String datafilepath = System.getProperty("user.dir") + "/src/test/resources/TData.xlsx";
 	public static final String reportfilepath = System.getProperty("user.dir") + "/test-output/ExtentReport-Order.html";
 	
 	public static final String screenshotpath = System.getProperty("user.dir") + "/Screenshots/";
 	
-	public static final String reportfilename = "C:\\Users\\daniel.r\\eclipse-workspace\\CucumberFramework\\src\\test-output\\ExtentReport.html";
+	public static final String reportfilename = System.getProperty("user.dir") + "src\\test-output\\ExtentReport.html";
 }
+
+//"C:\\Users\\daniel.r\\eclipse-workspace\\CucumberFramework\\src\\test\\resources\\TData.xlsx";
+//"C:\\Users\\daniel.r\\eclipse-workspace\\CucumberFramework\\src\\test-output\\ExtentReport.html";
