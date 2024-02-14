d4ce1ed40f689f8e34d9e2a6b4fbe6098dddd8b5
abina1501006 <abina1501006@IN-TR5LPW110203>
2024-02-14 18:23:11 +0530

    Final commit

--- src/main/java/utils/Test_Data.java
+++ src/main/java/utils/Test_Data.java
@@ -10,7 +10,7 @@ import constants.Constants;
 
 public class Test_Data {
 
-	public static String Datasheet(String sheetname, int rows, int columns) throws Exception{
+	public static String Datasheet(String sheetname, int rows, int columns) throws Exception {
 		Object result = new Object();
 		File location = new File(Constants.datafilepath);
 		FileInputStream fin = new FileInputStream(location);
@@ -18,9 +18,7 @@ public class Test_Data {
 		XSSFSheet sheet = workbook.getSheet(sheetname);
 		XSSFRow row1 = sheet.getRow(rows);
 		XSSFCell cell2 = row1.getCell(columns);
-		//String value=cell2.getCellValue();
-		
-		System.out.println(cell2);
+
 		if (cell2 != null) {
 			switch (cell2.getCellType()) { // to get different types of cell values
 			case NUMERIC:// numeric value in excel
@@ -34,38 +32,28 @@ public class Test_Data {
 			case BOOLEAN: // boolean value in excel
 				result = cell2.getBooleanCellValue();
 				break;
-			
-			
+
 			case BLANK: // blank value in excel
 				result = cell2.getStringCellValue();
 				break;
 			default:
 				break;
 			}
-		
-		//CellType ctype = cell2.getCellType();
-		/*System.out.println("ctype" + ctype);
-		if (ctype = "STRING") {
-			value = cell.getStringCellValue();
-		} else if (ctype == 0) {
-			double num = cell.getNumericCellValue();
-			System.out.println("Number is" + num);
-			value = String.valueOf(num);
-			System.out.println("string  value is" + value);
-		} else {
-			Date dateCell = cell.getDateCellValue();
-			SimpleDateFormat dateFormat = new SimpleDateFormat("dd-mm-yyyy");
-			value = dateFormat.format(dateCell);
+
+			// CellType ctype = cell2.getCellType();
+			/*
+			 * System.out.println("ctype" + ctype); if (ctype = "STRING") { value =
+			 * cell.getStringCellValue(); } else if (ctype == 0) { double num =
+			 * cell.getNumericCellValue(); System.out.println("Number is" + num); value =
+			 * String.valueOf(num); System.out.println("string  value is" + value); } else {
+			 * Date dateCell = cell.getDateCellValue(); SimpleDateFormat dateFormat = new
+			 * SimpleDateFormat("dd-mm-yyyy"); value = dateFormat.format(dateCell); } }
+			 */
+
+			/*
+			 * String value=cell2.getStringCellValue(); return value;
+			 */
 		}
-	}*/
-		
-		/*String value=cell2.getStringCellValue();
-		return value;*/
-}
 		return result.toString();
 	}
 }
-
-    
-		
-	
