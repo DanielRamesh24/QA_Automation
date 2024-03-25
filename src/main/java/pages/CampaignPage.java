f2b75ca7e20832d4473cd9773a1b6825330b7a2c
YuvasriAkila <58624678+YuvasriAkila@users.noreply.github.com>
2024-03-25 19:51:27 +0530

    lead & campaign page update delete

--- src/main/java/pages/CampaignPage.java
+++ src/main/java/pages/CampaignPage.java
@@ -36,6 +36,24 @@ public class CampaignPage extends DriverClass {
 
 	@FindBy(how = How.XPATH, using = "//button[@title='Save']")
 	public WebElement SaveBtn;
+	
+	@FindBy(how = How.XPATH, using = "//tbody/tr[1]/th[1]/span[1]")
+	public WebElement firstRecord;
+	
+	
+	@FindBy(how = How.XPATH, using = "//header/div[2]/div[1]/div[2]/ul[1]/li[4]/div[1]/div[1]/div[1]/div[1]/a[1]")
+	public WebElement dropdown;
+	
+	@FindBy(how = How.XPATH, using = "/html[1]/body[1]/div[8]/div[1]/ul[1]/li[2]/a[1]")
+	public WebElement edit;
+	
+	@FindBy(how = How.XPATH, using = "/html[1]/body[1]/div[8]/div[1]/ul[1]/li[3]/a[1]")
+	public WebElement delete;
+    
+	@FindBy(how = How.XPATH, using = "//span[contains(text(),'Delete')]")
+	public WebElement deleteyes;
+	
+
 
 	public void clickCampaign() throws InterruptedException {
 		Thread.sleep(10000);
@@ -46,6 +64,7 @@ public class CampaignPage extends DriverClass {
 
 	public void createcampaign() throws Exception {
 
+		
 		baseclass.presenceOfElement(driver, By.xpath(
 				"//img[@src='https://mastek-dev-ed.develop.my.salesforce.com/img/icon/t4v35/standard/campaign_120.png']"));
 		NewCampgn.click();
@@ -54,6 +73,34 @@ public class CampaignPage extends DriverClass {
 		CampaignName.sendKeys(Test_Data.Datasheet("Campaign", 1, 0));
 		ActiveChkbox.click();
 		SaveBtn.click();
-	}
+		baseclass.elementLgreenHighlight(CampaignName);
 
+		baseclass.staticWait(10);
+
+		
+	}
+	
+	public void editcampaign() throws Exception
+	{
+		baseclass.staticWait(5);
+		dropdown.click();	
+		edit.click();
+		CampaignName.sendKeys(Test_Data.Datasheet("Campaign", 1, 0));
+		SaveBtn.click();
+		baseclass.staticWait(10);
+		baseclass.elementYellowHighlight(CampaignName);
+
+	}
+	 
+	 public void deletecampaign() throws InterruptedException
+	 {
+			baseclass.staticWait(5);
+
+			dropdown.click();	
+			delete.click();
+			deleteyes.click();
+			baseclass.staticWait(5);
+
+		
+	 }
 }
